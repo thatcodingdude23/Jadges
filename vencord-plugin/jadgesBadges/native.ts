1bae8c8ea8c6cde027ebe57a272243cadc52b40a
thatcodingdude <152893832+thatcodingdude23@users.noreply.github.com>
2026-07-31 17:44:59 +0400

    Allow Discord media badge images in Vencord

--- vencord-plugin/jadgesBadges/native.ts
+++ vencord-plugin/jadgesBadges/native.ts
@@ -3,5 +3,7 @@
 import { CspPolicies, ImageSrc } from "@main/csp";
 
 CspPolicies["jadges.onrender.com"] = ImageSrc;
+CspPolicies["cdn.discordapp.com"] = ImageSrc;
+CspPolicies["media.discordapp.net"] = ImageSrc;
 CspPolicies["discord.fandom.com"] = ImageSrc;
 CspPolicies["static.wikia.nocookie.net"] = ImageSrc;
