eb9637f1245e83d1bf3790df532ada719c238af2
thatcodingdude <152893832+thatcodingdude23@users.noreply.github.com>
2026-08-02 06:00:13 +0400

    Enable website badge deletion integration

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { mkdir } from "node:fs/promises";
+import { installBadgeDeleteIntegration } from "./badgeDeleteIntegration.js";
 import { installBrandIntegration } from "./brandIntegration.js";
 import { config } from "./config.js";
 import { installDesktopThemeIntegration } from "./desktopThemeIntegration.js";
@@ -15,6 +16,7 @@ import { installWebsiteIntegration } from "./websiteIntegration.js";
 
 await mkdir(config.imagesDir, { recursive: true });
 installFinalAssetIntegration();
+installBadgeDeleteIntegration();
 installHiddenDashboardIntegration();
 installVisibilityIntegration();
 installProfileVisibilityReportIntegration();
