1b5c532bbc4d09634b7558d65c5db63c0d9f9bb3
thatcodingdude <152893832+thatcodingdude23@users.noreply.github.com>
2026-08-05 19:52:18 +0400

    Serve v47 update

--- src/vencordUpdateDashboardIntegration.ts
+++ src/vencordUpdateDashboardIntegration.ts
@@ -1,6 +1,6 @@
 import http, { type RequestListener, type ServerResponse } from "node:http";
 
-const UPDATE_VERSION = 46;
+const UPDATE_VERSION = 47;
 let installed = false;
 
 function sendManifest(response: ServerResponse): void {
