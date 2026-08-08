af19d2436e1480489e9bfecfaa8acefe122af007
thatcodingdude <152893832+thatcodingdude23@users.noreply.github.com>
2026-08-08 06:29:27 +0400

    Release Jadges desktop v51

--- src/vencordUpdateDashboardIntegration.ts
+++ src/vencordUpdateDashboardIntegration.ts
@@ -1,6 +1,6 @@
 import http, { type RequestListener, type ServerResponse } from "node:http";
 
-const UPDATE_VERSION = 50;
+const UPDATE_VERSION = 51;
 let installed = false;
 
 function sendManifest(response: ServerResponse): void {
